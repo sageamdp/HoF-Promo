33dee16bea2fa9cb419371035288e8e42dcf6ece
Daniel <danp@propaganda3.com>
2026-06-22 23:28:36 -0500

    Adding netlify packages

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,13 @@
 import { reactRouter } from "@react-router/dev/vite";
 import { defineConfig } from "vite";
-import devtoolsJson from 'vite-plugin-devtools-json'
 import tsconfigPaths from "vite-tsconfig-paths";
+// ↓ add this
+import netlifyReactRouter from "@netlify/vite-plugin-react-router";
 
 export default defineConfig({
-  plugins: [devtoolsJson(), reactRouter(), tsconfigPaths()],
+  plugins: [
+    reactRouter(),
+    tsconfigPaths(),
+    netlifyReactRouter() // ← add this
+  ]
 });
